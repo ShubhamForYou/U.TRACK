21ed81eaca54deaed8d10391b5d0ea0b0ba807c0
Shubham Sharma <samshubham142@gmail.com>
2024-12-06 19:09:26 +0530

    add log-in and sing-in user feature

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,9 @@
+const { findOne } = require("../models/url");
 const userModel = require("../models/user");
 const singIn = async (req, res) => {
   const { name, username, email, password } = req.body;
   if (!name || !username || !email || !password) {
-    res.status(200);
+    res.status(400);
     throw new Error("all fields are mandatory");
   }
   const newUser = await userModel.create({
@@ -13,4 +14,25 @@ const singIn = async (req, res) => {
   });
   res.status(201).json({ msg: `${newUser.name} welcome to U.track` });
 };
-module.exports = singIn;
+
+const logIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Both fields are mandatory");
+  }
+  const flag = await userModel.findOne({ email });
+  if (!flag) {
+    res.status(404);
+    throw new Error("user not registered");
+  }
+
+  const user = await userModel.findOne({ email, password });
+  if (!user) {
+    res.status(401);
+    throw new Error("email or password is invalid");
+  }
+  res.status(200).json({ msg: `${user.name} log-in to U.track successfully` });
+};
+module.exports = { singIn, logIn };
--- routers/user.js
+++ routers/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const singIn = require("../controllers/user");
+const { singIn, logIn } = require("../controllers/user");
 const router = express.Router();
 
 router.post("/singIn", singIn);
+router.post("/logIn", logIn);
 module.exports = router;
